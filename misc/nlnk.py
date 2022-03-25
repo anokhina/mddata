eb0af9500fdd6de3b5354455aa5fc6739b421b9f
Veronica Anokhina <anokhina.veronica@gmail.com>
2022-03-25 21:09:10 +0300

    add clear button

--- misc/nlnk.py
+++ misc/nlnk.py
@@ -100,14 +100,37 @@ def saveForm():
     else:
         print(templ.format(title = title, author = author, description = dsc, tags = tagsStr, url = urlStr, content = contentStr, date = date))
 
-  
+
+erasers = []
 def clearForm():
-    txtUrl.delete(0, tk.END)
-    txtContent.delete(0, tk.END)
-    txtTags.selection_clear(0, tk.END)
-    txtTitle.delete(0, tk.END)
-    txtAuthor.delete(0, tk.END)
-    txtDescription.delete("1.0", tk.END)
+    for e in erasers:
+        e.clear_text()
+
+class Eraser():
+    def __init__(self, entry, window):
+        erasers.append(self)
+        self.entry = entry
+        self.button = tk.Button(window, text="x", command=self.clear_text, takefocus=0)
+
+    def btn(self):
+        return self.button
+
+    def clear_text(self):
+        self.entry.delete(0, 'end')
+
+class EraserList(Eraser):
+    def __init__(self, entry, window):
+        super().__init__(entry, window)
+
+    def clear_text(self):
+        self.entry.selection_clear(0, tk.END)
+
+class EraserText(Eraser):
+    def __init__(self, entry, window):
+        super().__init__(entry, window)
+
+    def clear_text(self):
+        self.entry.delete("1.0", tk.END)
 
 window = tk.Tk()  
 window.title("Создать описание ссылки или файла")  
@@ -115,24 +138,29 @@ window.geometry('1024x800')
 
 wd = 40
 
+entryCol = 2;
 row = 0
 lblUrl = tk.Label(window, text="Url")
 lblUrl.grid(column=0, row=row)  
 txtUrl = tk.Entry(window,width=wd)  
-txtUrl.grid(column=1, row=row)  
+txtUrl.grid(sticky="w", column=entryCol, row=row)  
+Eraser(txtUrl, window).btn().grid(column=1, row=row)  
 
 row += 1
 lblContent = tk.Label(window, text="Content filename")
 lblContent.grid(column=0, row=row)  
 txtContent = tk.Text(window, height=8)  
-txtContent.grid(column=1, row=row)  
+txtContent.grid(sticky="w", column=entryCol, row=row)  
+EraserText(txtContent, window).btn().grid(column=1, row=row)  
 
 row += 1
 
-lblTags = tk.Label(window, text="Tags (space separated)")
+lblTags = tk.Label(window, text="Tags")
 lblTags.grid(column=0, row=row)  
 txtTags = tk.Listbox(window, width=wd, selectmode="extended")
-txtTags.grid(column=1, row=row)  
+txtTags.grid(sticky="w", column=entryCol, row=row)  
+EraserList(txtTags, window).btn().grid(column=1, row=row)  
+
 for i in ("css", 
     "dog", 
     "git", 
@@ -140,7 +168,10 @@ for i in ("css",
     "html", 
     "Java", 
     "JavaScript", 
+    "JavaSpring", 
+    "maven", 
     "music", 
+    "net", 
     "node", 
     "patterns", 
     "programming", 
@@ -160,19 +191,22 @@ row += 1
 lblTitle = tk.Label(window, text="Title")
 lblTitle.grid(column=0, row=row)  
 txtTitle = tk.Entry(window,width=wd)  
-txtTitle.grid(column=1, row=row)  
+txtTitle.grid(sticky="w", column=entryCol, row=row)  
+Eraser(txtTitle, window).btn().grid(column=1, row=row)  
 
 row += 1
 lblAuthor = tk.Label(window, text="Author")
 lblAuthor.grid(column=0, row=row)  
 txtAuthor = tk.Entry(window,width=wd)  
-txtAuthor.grid(column=1, row=row)  
+txtAuthor.grid(sticky="w", column=entryCol, row=row)  
+Eraser(txtAuthor, window).btn().grid(column=1, row=row)  
 
 row += 1
 lblDescription = tk.Label(window, text="Description")
 lblDescription.grid(column=0, row=row)  
 txtDescription = tk.Text(window, height=8)  
-txtDescription.grid(column=1, row=row)  
+txtDescription.grid(sticky="w", column=entryCol, row=row)  
+EraserText(txtDescription, window).btn().grid(column=1, row=row)  
 
 row += 1
 lblResult = tk.Label(window, text="Result")
